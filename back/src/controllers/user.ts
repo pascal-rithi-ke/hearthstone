83a6ea5811222243ab6152c4d756d542cc7cc001
bflorestal <yayou7710@hotmail.fr>
2022-12-22 23:56:37 +0100

    Connexion avec cookie

--- back/src/controllers/user.ts
+++ back/src/controllers/user.ts
@@ -31,14 +31,11 @@ exports.signup = async (req: Request, res: Response) => {
 
     // TODO: enlever le any
     db.query(selectRequest, [username], async (err: Error, result: any) => {
-      if (err) {
-        res.status(500).json({ err });
-        throw err;
-      }
+      if (err) res.status(500).json({ err });
 
       if (result.length > 0) {
+        console.log("on est ici");
         res.status(400).json({ message: "Utilisateur déjà existant" });
-        throw err;
       }
     });
 
@@ -47,8 +44,9 @@ exports.signup = async (req: Request, res: Response) => {
 
     // TODO: enlever le any
     db.query(insertRequest, [user], (err: Error, result: any) => {
-      if (!result) {
-        res.status(200).json({ message: "Utilisateur déjà existant" });
+      if (result.length === 0) {
+        console.log("en fait on est plutôt ici");
+        res.status(400).json({ message: "Utilisateur déjà existant" });
       } else {
         res.status(201).json({ message: "Utilisateur créé" });
       }
@@ -56,6 +54,55 @@ exports.signup = async (req: Request, res: Response) => {
   } catch (error) {
     console.log(error);
     res.status(400).json({ error });
+  }
+};
+
+exports.login = async (req: Request, res: Response) => {
+  try {
+    const { username, password: clearPassword } = req.body;
+
+    if (!username || !clearPassword) {
+      return res
+        .status(400)
+        .json({ message: "Veuillez remplir tous les champs" });
+    }
+
+    const db = dbConfig.getDB();
+
+    // Requête SQL
+    const sql = `SELECT * FROM players WHERE username = ?`;
+    // TODO: Enlever le any
+    db.query(sql, [username], async (err: Error, result: any) => {
+      if (err) res.status(404).json({ err });
+
+      if (result[0]) {
+        const { id, password: hashedPassword } = result[0];
+
+        const match = await bcrypt.compare(clearPassword, hashedPassword);
+
+        if (match) {
+          const maxAge = 3 * 24 * 60 * 60 * 1000;
+          const createToken = (id: number) => {
+            return jwt.sign({ id }, process.env.TOKEN_SECRET, {
+              expiresIn: maxAge,
+            });
+          };
+
+          const token = createToken(id);
+
+          res.cookie("jwt", token, {
+            httpOnly: true,
+            maxAge: maxAge,
+          });
+        }
+
+        res.status(200).json(result);
+      } else {
+        res.status(400).json({ message: "Vos identifiants sont erronés" });
+      }
+    });
+  } catch (err) {
+    res.status(200).json({ message: "Échec de la connexion" });
   }
 };
 
