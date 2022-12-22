3617d82c0841f5e2e8d0201ab3a94fc81f3b1fc3
bflorestal <yayou7710@hotmail.fr>
2022-12-22 20:38:22 +0100

    Amélioration forms login register

--- front/src/pages/Register.tsx
+++ front/src/pages/Register.tsx
@@ -6,7 +6,9 @@ export default function Register() {
     const formData = new FormData(form);
     const { username, password } = Object.fromEntries(formData.entries());
 
-    fetch("http://localhost:5000/api/user/signup", {
+    if (!username || !password) return;
+
+    fetch(`${import.meta.env.VITE_API_URL}/user/signup`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -28,11 +30,24 @@ export default function Register() {
         <form onSubmit={handleSubmit}>
           <div>
             <label htmlFor="username">Nom d'utilisateur</label>
-            <input type="text" id="username" name="username" />
+            <input
+              type="text"
+              id="username"
+              name="username"
+              minLength={3}
+              required
+            />
           </div>
           <div>
             <label htmlFor="password">Mot de passe</label>
-            <input type="password" id="password" name="password" />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              minLength={6}
+              autoComplete="new-password"
+              required
+            />
           </div>
 
           <button type="submit">S'inscrire</button>
