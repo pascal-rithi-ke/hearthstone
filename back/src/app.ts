ed5d1a96faef485ace94e438afcd79182d813337
bflorestal <yayou7710@hotmail.fr>
2022-12-22 15:14:01 +0100

    Connexion à la DB et ajout signup

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,14 +1,14 @@
 import express from "express";
 import helmet from "helmet";
 // import cors from "cors";
+import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 
 // Imports des routes
 const helloRoute = require("./routes/hello");
 const userRoute = require("./routes/user");
 
-// Importe le fichier de config de la connexion à la bdd
-// const dbConfig = require("./config/db");
+const dbConfig = require("./config/db");
 
 require("dotenv").config();
 
@@ -16,8 +16,8 @@ const app = express();
 
 app.use(helmet());
 // app.use(cors);
-// app.use(express.json());
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Routes
