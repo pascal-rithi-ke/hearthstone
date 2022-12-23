86c1000009a0adb81601699bee48218bbccd6abc
bflorestal <yayou7710@hotmail.fr>
2022-12-23 13:44:52 +0100

    Début model User

---
+++ back/src/model/User.ts
@@ -0,0 +1,19 @@
+export class User {
+  private id?: number;
+  private username: string;
+  private password?: string;
+
+  constructor(id: number, username: string, password?: string) {
+    this.id = id;
+    this.username = username;
+    this.password = password;
+  }
+
+  public getUsername(): string {
+    return this.username;
+  }
+
+  public setUsername(username: string): void {
+    this.username = username;
+  }
+}
