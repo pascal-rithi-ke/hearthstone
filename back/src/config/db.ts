8af9110e2e3c3338710de0b5cd2d66fc24114a39
bflorestal <yayou7710@hotmail.fr>
2022-12-23 15:39:06 +0100

    db est de retour

--- back/src/config/db.ts
+++ back/src/config/db.ts
@@ -2,13 +2,15 @@ require("dotenv").config({ path: "./src/config/.env" });
 
 const mysql = require("mysql2");
 
-const db = mysql.createPool({
+// Paramètres de connexion à la base de données
+const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
+
 // export pour utiliser cette connexion
 module.exports.getDB = () => {
-  return db.promise();
+  return db;
 };
